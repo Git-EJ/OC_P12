dc78416bca26f9f4136010ec0a275f8a84275000
EJ <eric_j_fra@yahoo.fr>
2023-11-13 11:25:04 +0100

    Changes:    - UserTodayScore css and text contents

--- src/molecules/UserTodayScore/index.jsx
+++ src/molecules/UserTodayScore/index.jsx
@@ -1,57 +1,65 @@
-import {RadialBarChart, RadialBar, Legend, Tooltip, ResponsiveContainer } from 'recharts';
+import {RadialBarChart, RadialBar, ResponsiveContainer } from 'recharts';
 import PropTypes from 'prop-types'
 import Root from './styled';
 
 const UserTodayScore = ({ todayScore }) => {
 
   if (!todayScore || todayScore > 1) return (<> </>)
-  console.log('TD',todayScore)
+  // console.log('TD',todayScore)
 
+  const score = todayScore * 100
   const scoreData = [{value : todayScore}]
   const scoreDataToAngle = (todayScore * 360)
   const x = (-720 * todayScore + 90)
 
-  console.log('SD', scoreData);
-  console.log('EA', scoreDataToAngle);
-  console.log('X', x);
+  // console.log('SD', scoreData);
+  // console.log('EA', scoreDataToAngle);
+  // console.log('X', x);
+
 
 
   if (!scoreData || !scoreDataToAngle || !x) return (<> </>)
 
   return (
     <Root className='user_todayscore_container'>
+
+      <span className='user_today_score_title'>Score</span>
+
+      <div className='user_todayscore_center_container'>
+        <div className='user_todayscore_center_score'>{`${score}%`}</div>
+        <div className='user_todayscore_center_text'>de votre objectif</div>
+      </div>
+      
+
       <ResponsiveContainer width='100%' height='100%'>
       
         <RadialBarChart data={scoreData}
-                        // width={730} 
-                        // height={250} 
-                        innerRadius="60%" 
-                        outerRadius="80%" 
+                        innerRadius="77%" 
+                        outerRadius="90%" 
                         startAngle={90} 
-                        // endAngle={-270}
                         endAngle={scoreDataToAngle + x}
+                        // endAngle={-270}                    // TODO REMOVE ALL
                         // endAngle={scoreDataToAngle + 90}   // total= +90    EA= 0
                         // endAngle={scoreDataToAngle - 90}   // total= 0      EA= 0.25
                         // endAngle={scoreDataToAngle - 270}  // total= -90    EA= 0.5
                         // endAngle={scoreDataToAngle - 450}  // total= -180   EA= 0.75
                         // endAngle={scoreDataToAngle - 630}  // total= -270   EA= 1
                         fill = "#FF0101"
-                        radius={[10, 10, 0, 0]} 
+                        isAnimationActive={true} 
+                        animationBegin={0}        
+                        animationDuration={2000} 
+                        animationEasing="ease-out"
           >
 
           <RadialBar  dataKey= 'value'
                       cornerRadius="50%" 
                       minAngle={15} 
-                      label={{ fill: 'lime', position: 'insideStart' }} 
-                      isAnimationActive={true} 
-                      // animationEasing='ease-out'
-                      background clockWise={true} 
-            />
+                      // label={{ fill: 'lime', position: 'insideStart' }} 
+                      isAnimationActive={false} 
+                      clockWise={true} 
+                     
+          />
         
-          {/* <Legend iconSize={10} width={120} height={140} layout='vertical' verticalAlign='middle' align="right" /> */}
-        
-          {/* <Tooltip /> */}
-
         </RadialBarChart>
       </ResponsiveContainer>
     </Root>
--- src/molecules/UserTodayScore/styled.jsx
+++ src/molecules/UserTodayScore/styled.jsx
@@ -5,7 +5,53 @@ const Root = styled.div`
   &.user_todayscore_container {
     width:100%;
     height:100%;
+    position: relative;
   }
+
+  & .user_today_score_title {
+    position: absolute;
+    top: 7%;
+    left: 5%;
+    font-size = clamp(0.7rem, 0.9vw, 0.9rem);
+    font-weight: 500;
+  }
+
+
+  & .user_todayscore_center_container {
+    width:69%;
+    height: 0;
+    padding-top: 69%; //same value as width
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    // background: pink; //DEV
+    background: #fff; // TODO
+    border-radius: 50%;
+  }
+
+  & .user_todayscore_center_score {
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    font-size: clamp(1rem, 1.7vw, 1.7rem);
+    font-weight: 700;
+  }
+
+  & .user_todayscore_center_text {
+    position: absolute;
+    top: 60%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    font-size: clamp(0.7rem, 1vw, 1rem);
+    font-weight: 500;
+    color: #74798C;
+  }
+
+
 `
 
 export default Root
